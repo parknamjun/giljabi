bfe67541b169340adb6d51a3bf32ebe477f51cda
parknamjun <eahn.park@gmail.com>
2022-03-26 14:32:22 +0900

    경로 탐색 테스트 코드 추가

--- src/test/java/kr/giljabi/api/request/RequestRouteDataTest.java
+++ src/test/java/kr/giljabi/api/request/RequestRouteDataTest.java
@@ -24,7 +24,7 @@ class RequestRouteDataTest {
     GET http://localhost:8080/api/1.0/route?start=127.01117,37.5555&target=126.99448,37.54565&direction=cycling-road
 */
     @Test
-    public void test() throws IOException {
+    public void openRouteTest1() throws IOException {
         String url = "http://localhost:8080/api/1.0/route?start=127.01117,37.5555&target=126.99448,37.54565&direction=cycling-road";
 
         String result = MyHttpUtils.httpGetMethod(url);
