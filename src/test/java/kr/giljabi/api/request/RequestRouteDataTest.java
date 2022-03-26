04d8afd3b36a69ac5249c6299cd0d3905adde239
parknamjun <eahn.park@gmail.com>
2022-03-26 14:28:22 +0900

    경로 탐색 테스트 코드 추가

--- src/test/java/kr/giljabi/api/request/RequestRouteDataTest.java
+++ src/test/java/kr/giljabi/api/request/RequestRouteDataTest.java
@@ -1,21 +1,33 @@
 package kr.giljabi.api.request;
 
+import kr.giljabi.api.exception.GiljabiException;
+import kr.giljabi.api.utils.MyHttpUtils;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RequestRouteDataTest {
-
+    /*
+    GET http://localhost:8080/api/1.0/route?start=127.01117,37.5555&target=126.99448,37.54565&direction=cycling-road
+*/
     @Test
-    public void test() {
-        String txt = "1212.11,22222.11111";
-        Double[] test = Stream.of(txt.split(","))
-                .mapToDouble(Double::parseDouble)
-                .boxed().toArray(Double[]::new);
-        System.out.println("test = " + test[0]);
-        System.out.println("test = " + test[1]);
+    public void test() throws IOException {
+        String url = "http://localhost:8080/api/1.0/route?start=127.01117,37.5555&target=126.99448,37.54565&direction=cycling-road";
+
+        String result = MyHttpUtils.httpGetMethod(url);
+        System.out.println("result = " + result);
     }
 }
