020711eccbead4cb111042ceacaca8e980a74170
parknamjun <eahn.park@gmail.com>
2024-03-22 15:43:32 +0900

    Database 사용삭제

--- src/main/java/kr/giljabi/api/entity/BaseEntity.java
+++ src/main/java/kr/giljabi/api/entity/BaseEntity.java
@@ -1,6 +1,6 @@
 package kr.giljabi.api.entity;
 
-import lombok.Getter;
+/*import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -10,12 +10,15 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import java.time.LocalDateTime;*/
 
+/*
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
+*/
 public abstract class BaseEntity {
+/*
 
     @CreatedBy
     @Column(updatable = false)
@@ -35,4 +38,6 @@ public abstract class BaseEntity {
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 
+}
+*/
 }
